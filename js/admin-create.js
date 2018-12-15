686e60981aaccc85a746e91fb4a28b503aa60e06
Jin Hang <jinhang@nuaa.edu.cn>
2018-12-15 18:17:11 +0800

    Completed adding students (single and batch)

--- js/admin-create.js
+++ js/admin-create.js
@@ -2,29 +2,24 @@
 var vm = new Vue({
   el:'#usermanage',
   data:{
-    user: {
-      username: '', password: '', usertype: ''
-    },
-    userlist: [
-      {username: 'user1', password: 'u1', usertype: 'student'},
-      {username: 'user2', password: 'u2', usertype: 'teacher'},
-    ],
-    postdata: {
-      username: '',
-      password: '',
-      usertype: '',
-      batch_names: ''
-    },
+    username: '',
+    password: '',
+    usertype: '',
+    batch_names: ''
   },
   methods:{
     insert:function(){
-      console.log(this.postdata);
-      if (this.postdata.username == '' || this.postdata.password == '' || this.postdata.usertype == '')
+      postdata = {
+        username: this.username,
+        password: this.password,
+        usertype: this.usertype
+      };
+      if (postdata.username == '' || postdata.password == '' || postdata.usertype == '')
       {
         alert('用户信息不能有缺省');
         return;
       }
-      this.$http.post(backend_server + 'user-add/', this.postdata, {emulateJSON: true, credentials: true}).then(function(res){
+      this.$http.post(backend_server + 'user-add/', postdata, {emulateJSON: true, credentials: true}).then(function(res){
         console.log(res.bodyText);
         var dataret = JSON.parse(res.bodyText);
         if (dataret.code == 200)
@@ -41,44 +36,19 @@ var vm = new Vue({
         alert('添加用户失败(unknown error)');
       });
     },
-    remove:function(username){
-      console.log(username + ' is to be deleted.');
-      this.$http.post(backend_server + 'user-delete/', {"username": username}, {emulateJSON: true, credentials: true}).then(function(res){
-        console.log(res.bodyText);
-        var dataret = JSON.parse(res.bodyText);
-        if (dataret.code == 200)
-        {
-          alert('删除用户成功');
-          location.reload();
-        }
-        else
-        {
-          alert('删除用户失败(' + dataret.info +')');
-        }
-      },function(res){
-        console.log(res.status);
-        alert('删除用户失败(未知错误)');
-      });
+    reset_batch:function(){
+      this.batch_names = '';
     },
-    get_userlist:function(){
-      this.$http.get(backend_server + 'user-list/', {credentials: true}).then(function(res){
-        console.log(res.bodyText);
-        var dataret = JSON.parse(res.bodyText);
-        if (dataret.code == 200)
-        {
-          this.userlist = dataret.userlist
-        }
-        else
-        {
-          alert('获取用户列表失败(' + dataret.info +')');
-        }
-      },function(res){
-        console.log(res.status);
-        alert('获取用户列表失败(未知错误)');
-      });
+    reset_form:function(){
+      this.username = '';
+      this.password = '';
+      this.usertype = '';
     },
     batch_insert:function(){
-      this.$http.post(backend_server + 'user-add-batch/', this.postdata, {emulateJSON: true, credentials: true}).then(function(res){
+      postdata = {
+        batch_names: this.batch_names
+      };
+      this.$http.post(backend_server + 'user-add-batch/', postdata, {emulateJSON: true, credentials: true}).then(function(res){
         console.log(res.bodyText);
         var dataret = JSON.parse(res.bodyText);
         if (dataret.code == 200)
@@ -97,6 +67,5 @@ var vm = new Vue({
     },
   },
   created:function(){
-    this.get_userlist();
   }
 })
