3d50315d2e3850ef3fabe4656c82a8e373c36faa
Jin Hang <jinhang@nuaa.edu.cn>
2018-12-25 20:52:47 +0800

    Completed querying students' final grades

--- js/test-taken.js
+++ js/test-taken.js
@@ -2,50 +2,29 @@
 var vm = new Vue({
   el:'#app',
   data:{
-    myinfo: '',
-    testlist : '',
-    testhistory: ''
+    takenlist : '',
   },
   methods:{
-    enter_test:function(paperid){
-      //window.open("testing.html?paperid=" + paperid);
-      window.location.href = "test-testing.html?paperid=" + paperid;
-    },
-    get_test_list:function(){
-      this.$http.get(backend_server + 'paper-get-list-stu/', {credentials: true})
+    get_taken_list:function(){
+      this.$http.get(backend_server + 'test-history/', {credentials: true})
       .then(function(res){
         console.log(res.bodyText);
         var dataret = JSON.parse(res.bodyText);
         if (dataret.code == 200)
         {
-          tests = dataret.list;
-          taken = dataret.taken;
-          for (var i=0; i<tests.length; i++)
-          {
-            for (var j=0; j<taken.length; j++)
-            {
-              if (tests[i]['pid'] == taken[j]['pid'])
-              {
-                tests[i]['taken'] = true;
-                break;
-              }
-            }
-          }
-          this.testlist = tests;
+          this.takenlist = dataret.list;
         }
         else
         {
-          alert('获取试题列表失败(1)');
+          alert('获取考试记录失败(1)');
         }
       },function(res){
         console.log(res.status);
-        alert('获取试题列表失败(2)');
+        alert('获取考试记录失败(2)');
       });
     },
-  }
+  },
   created:function(){
-    //this.get_my_info();
-    this.get_test_list();
-    //this.get_test_history();
+    this.get_taken_list();
   }
 })
